7597180f91771851b4d017413b6a305353a2c7aa
Informática <Informtica@adolphoberezin.com>
2019-10-17 14:13:15 -0300

    arquivo que altera o registro no banco

---
+++ altera.php
@@ -0,0 +1,15 @@
+<?php
+    $conexao = mysqli_connect("localhost","id10374988_crudpizza","crudpizza","id10374988_crudpizza");
+    //                  onde esta o banco    usuario banco          senha        nome banco de dados
+
+    $codigo = $_POST['codigo'];
+    $sabor = $_POST['sabor'];
+    $valor = $_POST['valor'];
+
+
+
+    $query = "update tb_pizza set ds_sabor = '$sabor', vl_pizza = $valor where cd_pizza = $codigo";
+
+    mysqli_query($conexao,$query);
+
+    echo "Registro alterado com Sucesso!";
